98d111b32801098f181826cc5b416e16b441dd4b
VolodymyrHubych <volodymyr.hubych@devcom.com>
2017-11-02 13:55:47 +0200

    Change login component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  token_endpoint : 'http://63fa5f09.ngrok.io/token',
-  Url : 'http://63fa5f09.ngrok.io/api/'
+  token_endpoint : 'http://f40b61f9.ngrok.io/token',
+  Url : 'http://f40b61f9.ngrok.io.api/'
 };
